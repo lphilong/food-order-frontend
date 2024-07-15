ce330c67f509ae1df5e8c6857cc054384d1c2f99
Long <lpl141020@gmail.com>
2024-07-15 20:31:26 +0700

    update

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,72 +1,14 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import Layout from './layouts/layout';
-import HomePage from './pages/HomePage';
-import AuthCallbackPage from './pages/AuthCallbackPage';
-import UserProfilePage from './pages/UserProfilePage';
-import ProtectedRoute from './auth/ProtectedRoute';
-import ManageRestaurantPage from './pages/ManageRestaurantPage';
-import SearchPage from './pages/SearchPage';
-import DetailPage from './pages/DetailPage';
-import OrderStatusPage from './pages/OrderStatusPage';
+import { Routes, Route } from 'react-router-dom';
+import { mRoute } from './routes';
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route
-                path="/"
-                element={
-                    <Layout showHero>
-                        <HomePage />
-                    </Layout>
-                }
-            />
-            <Route path="/auth-callback" element={<AuthCallbackPage />} />
-            <Route
-                path="/search/:city"
-                element={
-                    <Layout showHero={false}>
-                        <SearchPage />
-                    </Layout>
-                }
-            />
-            <Route
-                path="/detail/:restaurantId"
-                element={
-                    <Layout showHero={false}>
-                        <DetailPage />
-                    </Layout>
-                }
-            />
-            <Route element={<ProtectedRoute />}>
-                <Route
-                    path="/order-status"
-                    element={
-                        <Layout>
-                            <OrderStatusPage />
-                        </Layout>
-                    }
-                />
-                <Route
-                    path="/user-profile"
-                    element={
-                        <Layout>
-                            <UserProfilePage />
-                        </Layout>
-                    }
-                />
-                <Route
-                    path="/manage-restaurant"
-                    element={
-                        <Layout>
-                            <ManageRestaurantPage />
-                        </Layout>
-                    }
-                />
-            </Route>
-
-            <Route path="*" element={<Navigate to="/" />} />
+            {mRoute.map((route, index) => {
+                const Page = route.element;
+                return <Route key={index} path={route.path} element={Page} />;
+            })}
         </Routes>
     );
 };
-
 export default AppRoutes;
