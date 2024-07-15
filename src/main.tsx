a3e159dde77c3a824602acb742dee721f5387d62
Long <lpl141020@gmail.com>
2024-07-15 20:31:01 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './global.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { Toaster } from './components/ui/sonner';
+import { Toaster } from 'sonner';
+
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
@@ -13,13 +15,14 @@ const queryClient = new QueryClient({
         },
     },
 });
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Router basename="/">
+        <Router>
             <QueryClientProvider client={queryClient}>
                 <Auth0ProviderWithNavigate>
                     <AppRoutes />
-                    <Toaster visibleToasts={1} position= "top-right" richColors/>
+                    <Toaster visibleToasts={1} position="top-right" richColors />
                 </Auth0ProviderWithNavigate>
             </QueryClientProvider>
         </Router>
