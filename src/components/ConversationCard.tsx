67ac16d17fabdce42123de4a6e20cec89c312df4
Long <lpl141020@gmail.com>
2024-08-20 22:17:39 +0700

    update

--- src/components/ConversationCard.tsx
+++ src/components/ConversationCard.tsx
@@ -6,10 +6,9 @@ import { useEffect, useState } from 'react';
 type Props = {
     restaurant?: Restaurant;
     link: string;
-    hasNewMessage: boolean;
 };
 
-const ConversationCard = ({ restaurant, link, hasNewMessage }: Props) => {
+const ConversationCard = ({ restaurant, link }: Props) => {
     const [imageSrc, setImageSrc] = useState(restaurant?.imageUrl || 'placeholder-image-url');
 
     useEffect(() => {
@@ -29,13 +28,13 @@ const ConversationCard = ({ restaurant, link, hasNewMessage }: Props) => {
                     <img src={imageSrc} className="rounded-md w-full h-full object-cover" />
                 </AspectRatio>
                 <h3 className="text-2xl font-bold tracking-tight mb-2 group-hover:underline text-center">{restaurant.restaurantName}</h3>{' '}
-                {hasNewMessage && (
+                {/* {hasNewMessage && (
                     <div className="absolute top-2 right-2">
                         <div className="p-2 bg-gradient-to-r from-red-500 to-pink-500 rounded-full shadow-lg animate-scaleUp">
                             <h1 className="text-md font-extrabold text-center text-white ">New</h1>
                         </div>
                     </div>
-                )}
+                )}*/}
             </Link>
         </div>
     );
