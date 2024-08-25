34a6f1478a28af403227e13c435f5c9dc57e28f5
Long <lpl141020@gmail.com>
2024-08-26 01:56:15 +0700

    update load old messages

--- src/api/ChatApi.tsx
+++ src/api/ChatApi.tsx
@@ -1,32 +1,56 @@
 import { LastMessage, Message, NewMessage } from '@/types';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useCallback, useState } from 'react';
 import { useMutation, useQuery } from 'react-query';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 //get message between user and restaurant
-export const useGetMessage = (restaurantId: string, userId: string) => {
+export const useGetMessages = (restaurantId: string, userId: string) => {
     const { getAccessTokenSilently } = useAuth0();
+    const [loading, setLoading] = useState(false);
 
-    const getMessageRequest = async (): Promise<Message[]> => {
-        const accessToken = await getAccessTokenSilently();
+    const getMessagesRequest = useCallback(
+        async (before?: string) => {
+            setLoading(true);
+            try {
+                const accessToken = await getAccessTokenSilently();
+                const response = await fetch(`${API_BASE_URL}/api/chat/${restaurantId}/${userId}?limit=20${before ? `&before=${before}` : ''}`, {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                });
 
-        const response = await fetch(`${API_BASE_URL}/api/chat/${restaurantId}/${userId}`, {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-        });
+                if (!response.ok) {
+                    throw new Error('Failed to get chat messages');
+                }
 
-        if (!response.ok) {
-            throw new Error('Failed to get chat messages');
-        }
-        return response.json();
+                const messages = await response.json();
+                return messages;
+            } catch (error) {
+                console.error('Error fetching messages:', error);
+                return [];
+            } finally {
+                setLoading(false);
+            }
+        },
+        [restaurantId, userId, getAccessTokenSilently],
+    );
+
+    const { data: initialMessages = [], isLoading: initialLoading } = useQuery(
+        ['fetchChatMessages', restaurantId, userId],
+        () => getMessagesRequest(),
+        {
+            initialData: [],
+        },
+    );
+
+    return {
+        messages: initialMessages,
+        isLoading: initialLoading || loading,
+        getMessagesRequest,
     };
-
-    const { data: messages = [], isLoading } = useQuery(['fetchChatMessages', restaurantId, userId], getMessageRequest);
-
-    return { messages, isLoading };
 };
 
 export const useGetNewMessage = () => {
